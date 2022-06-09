334b4483dbfd0f85523fbfe6dab0b03fd98324d4
Krishna Mishra <kuleen24@gmail.com>
2022-06-10 00:05:43 +0530

    Add project files.

---
+++ LINQ/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+		{
+			List<int> elements = new List<int>()
+		{ 1,2,3,4,5,6,7,8,9,};
+
+			var Intlist = elements.Where(obj => obj % 2 == 0).ToList();
+
+			foreach (var i in Intlist)
+			{
+				Console.WriteLine(i);
+			}
+			Console.ReadKey();
+		}
+	}
+}
